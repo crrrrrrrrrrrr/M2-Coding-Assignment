6f8f4a86125fdc28cc19acefa676ca4d8563d9ff
crrrrrrrrrrrr <cynthiarong2012@gmail.com>
2025-10-18 00:27:36 -0400

    working on task 2, almost done, checking that its passing the tests now

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -31,7 +31,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
 
         // array list of subbreeds
         ArrayList<String> subbreeds = new ArrayList<>();
